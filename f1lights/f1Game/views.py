aeb0e48cf29be440758dd9f441ee3f9b8d110ea0
NathanCrowley <crowley2305@gmail.com>
2022-01-17 00:09:31 +0000

    tried to add feature to show error when stop pressed too early - doesnt work

--- f1lights/f1Game/views.py
+++ f1lights/f1Game/views.py
@@ -6,4 +6,15 @@ from .models import Score
 
 
 def index(request):
-    return render(request,'f1Game/f1Game.html')
+    score_list = Score.objects.order_by('high_score')[0]
+    list_of_scores = Score.objects.all()
+
+    '''if request.method == 'POST':
+        current_score = request.POST['current_score']
+        current = Score(high_score=current_score)
+        current.save()'''
+
+    context = {
+        'scores':score_list,
+    }
+    return render(request,'f1Game/f1Game.html', context)
